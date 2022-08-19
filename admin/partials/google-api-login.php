96465fac28fc9c8d2a31a9473d47e034bf004506
Jhom <jhomignacio08@gmail.com>
2022-08-19 11:49:55 +0800

    updated logo

--- admin/partials/google-api-login.php
+++ admin/partials/google-api-login.php
@@ -23,6 +23,7 @@ $gClient->setScopes(['https://www.googleapis.com/auth/analytics.readonly','https
 
 // login URL
 $login_url = $gClient->createAuthUrl();
+$img = plugin_dir_url( __DIR__ ).'assets/img/google-analytics-logo.png';
  ?>
  <style>
 .form-group input{
@@ -32,11 +33,13 @@ $login_url = $gClient->createAuthUrl();
     display: grid;
 }
 .login-analytic-data button{
-    padding: 10px;
-    background: #4c4fd7;
+	border-radius: 8px;
+    padding: 10px 50px;
+    background: linear-gradient(170deg,#ec1081 5%,#f05075 30%,#f16b63 48%,#f16b63 52%,#f48835 70%,#f6971c 95%);
     color: #fff;
+    border-color: #fff;
     border: none;
-    font-weight: bold;
+    font-weight: 700;
 }
 
 .login-analytic-data{
@@ -48,8 +51,11 @@ $login_url = $gClient->createAuthUrl();
 }
  </style>
 <form action='' method="POST" class="login-analytic-data">
+    <div class="analytics_logo">
+		<img src="<?php echo $img;?>">
+    </div>
     <p>Login to google account to generate access token. </p>
-    <button onclick="window.location = '<?php echo $login_url; ?>'" type="button" class="btn btn-danger">Login with Google</button>
+    <button onclick="window.location = '<?php echo $login_url; ?>'" type="button" class="btn login_analytics_api">Login with Google</button>
 </form>
 <?php
  
